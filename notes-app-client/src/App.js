7abfb1a6b4c5dd051714a1c622e275d7a3e04f9c
BudacaB <budaca.b@gmail.com>
2019-10-14 01:04:15 +0300

    load state from session ; clear session on logout

--- notes-app-client/src/App.js
+++ notes-app-client/src/App.js
@@ -4,21 +4,38 @@ import "./App.css";
 import Routes from "./Routes";
 import { LinkContainer } from "react-router-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Auth } from "aws-amplify";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      isAuthenticated: false
+      isAuthenticated: false,
+      isAuthenticating: true
     };
+  }
+
+  async componentDidMount() {
+    try {
+      await Auth.currentSession();
+      this.userHasAuthenticated(true);
+    } catch (e) {
+      if (e !== "No current user") {
+        alert(e);
+      }
+    }
+
+    this.setState({ isAuthenticating: false });
   }
 
   userHasAuthenticated = authenticated => {
     this.setState({ isAuthenticated: authenticated });
   };
 
-  handleLogout = event => {
+  handleLogout = async event => {
+    await Auth.signOut();
+
     this.userHasAuthenticated(false);
   };
 
@@ -29,23 +46,24 @@ class App extends Component {
     };
 
     return (
-      <div className="App container">
-        <Link to="/">Scratch</Link>
-        {this.state.isAuthenticated ? (
-          <div onClick={this.handleLogout}>Logout</div>
-        ) : (
-          <Fragment>
-            <LinkContainer to="/signup">
-              <div>Signup</div>
-            </LinkContainer>
-            <LinkContainer to="/login">
-              <div>Login</div>
-            </LinkContainer>
-          </Fragment>
-        )}
-
-        <Routes childProps={childProps} />
-      </div>
+      !this.state.isAuthenticating && (
+        <div className="App container">
+          <Link to="/">Scratch</Link>
+          {this.state.isAuthenticated ? (
+            <div onClick={this.handleLogout}>Logout</div>
+          ) : (
+            <Fragment>
+              <LinkContainer to="/signup">
+                <div>Signup</div>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <div>Login</div>
+              </LinkContainer>
+            </Fragment>
+          )}
+          <Routes childProps={childProps} />
+        </div>
+      )
     );
   }
 }
